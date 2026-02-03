a6716c7ce74a0177bb5c821025da5c0ed5f20a84
gpanakkal <111669555+gpanakkal@users.noreply.github.com>
2026-02-02 17:46:24 -0800

    Add tests for reviewing, skipping, dismissing, and un-dismissing articles

--- src/tests/e2e/core.spec.ts
+++ src/tests/e2e/core.spec.ts
@@ -11,9 +11,13 @@ import {
   openVault,
   shouldCleanup,
 } from '../e2e-setup/helpers';
+import { openNote, useCommandPalette } from './helpers';
 
 /**
- * Tests of core plugin functionality
+ * Tests of core plugin functionality:
+ * - Opening review interface
+ * - Importing articles
+ * -
  */
 
 let app: ElectronApplication;
@@ -34,10 +38,7 @@ test.afterEach(async () => {
 });
 
 test('Can open the review interface by executing the command', async () => {
-  await window.getByLabel('Open command palette', { exact: true }).click();
-  const commandPalette = window.locator(':focus');
-  await commandPalette.fill('Incremental Reading: Learn');
-  await commandPalette.press('Enter');
+  await useCommandPalette(window, 'Incremental Reading: Learn');
 
   // Verify the tab header for the review interface is visible
   await expect(
@@ -53,54 +54,232 @@ test('Can open the review interface from the ribbon button', async () => {
   ).toBeVisible();
 });
 
-test('can import articles from the file explorer context menu', async () => {
-  await window
-    .locator('div')
-    .filter({ hasText: /^sources$/ })
-    .nth(1)
-    .click();
+test.describe('Article Importing', () => {
+  test('can import Markdown from the file explorer context menu', async () => {
+    await window
+      .locator('div')
+      .filter({ hasText: /^sources$/ })
+      .nth(1)
+      .click();
 
-  await window.getByText('Curse of dimensionality - Wikipedia').click({
-    button: 'right',
+    await window.getByText('Curse of dimensionality - Wikipedia').click({
+      button: 'right',
+    });
+    await window.getByText('Import article').click();
+    await window.getByRole('button', { name: 'Import' }).click();
+    await window.getByLabel('Incremental Reading').click();
+
+    // look for the action bar to confirm we're in review
+    await expect(
+      window.getByRole('button', { name: 'Continue' })
+    ).toBeVisible();
+    await expect(
+      window.getByText('Curse of dimensionality - Wikipedia').nth(1)
+    ).toBeVisible();
   });
-  await window.getByText('Import article').click();
-  await window.getByRole('button', { name: 'Import' }).click();
-  await window.getByLabel('Incremental Reading').click();
 
-  // ensure the action bar is visible
-  await expect(window.getByRole('button', { name: 'Continue' })).toBeVisible();
-  await expect(
-    window.getByText('Curse of dimensionality - Wikipedia').nth(1)
-  ).toBeVisible();
+  test('can import Markdown from the note hamburger menu', async () => {
+    await openNote(
+      window,
+      'sources/Memorizing a programming language using spaced repetition'
+    );
+
+    await window.getByRole('button', { name: 'More options' }).click();
+    await window.getByText('Import article').click();
+    await window.getByRole('button', { name: 'Import' }).click();
+    await useCommandPalette(window, 'Incremental Reading: Learn');
+
+    // look for the action bar to confirm we're in review
+    await expect(
+      window.getByRole('button', { name: 'Continue' })
+    ).toBeVisible();
+    await expect(
+      window
+        .getByText(
+          'Memorizing a programming language using spaced repetition software'
+        )
+        .nth(1)
+    ).toBeVisible();
+  });
+
+  test('can import Markdown from the command palette', async () => {
+    await openNote(
+      window,
+      'sources/Memorizing a programming language using spaced repetition'
+    );
+
+    await useCommandPalette(window, 'Incremental Reading: Import Article');
+    await window.getByRole('button', { name: 'Import' }).click();
+    await useCommandPalette(window, 'Incremental Reading: Learn');
+
+    // look for the action bar to confirm we're in review
+    await expect(
+      window.getByRole('button', { name: 'Continue' })
+    ).toBeVisible();
+    await expect(
+      window
+        .getByText(
+          'Memorizing a programming language using spaced repetition software'
+        )
+        .nth(3)
+    ).toBeVisible();
+  });
 });
 
-test('can import articles from the note hamburger menu', async () => {
-  await window
-    .locator('div')
-    .filter({ hasText: /^sources$/ })
-    .nth(1)
-    .click();
+test.describe('Action Bar', () => {
+  test('Can review articles', async () => {
+    await openNote(
+      window,
+      'sources/Memorizing a programming language using spaced repetition'
+    );
 
-  await window
-    .locator('div')
-    .filter({
-      hasText:
-        /^Memorizing a programming language using spaced repetition software$/,
-    })
-    .nth(1)
-    .click();
-  await window.getByRole('button', { name: 'More options' }).click();
-  await window.getByText('Import article').click();
-  await window.getByRole('button', { name: 'Import' }).click();
-  await window.getByLabel('Incremental Reading').click();
+    await useCommandPalette(window, 'Incremental Reading: Import Article');
+    await window.getByRole('button', { name: 'Import' }).click();
+    await useCommandPalette(window, 'Incremental Reading: Learn');
+    await window.getByRole('button', { name: 'Continue' }).click();
 
-  // ensure the action bar is visible
-  await expect(window.getByRole('button', { name: 'Continue' })).toBeVisible();
-  await expect(
-    window
-      .getByText(
-        'Memorizing a programming language using spaced repetition software'
-      )
-      .nth(1)
-  ).toBeVisible();
+    await useCommandPalette(window, 'Close current tab');
+
+    await useCommandPalette(window, 'Incremental Reading: Learn');
+    await expect(
+      window
+        .getByText(
+          'Memorizing a programming language using spaced repetition software'
+        )
+        .nth(3)
+    ).not.toBeVisible();
+  });
+
+  test('Can skip items', async () => {
+    await openNote(
+      window,
+      'sources/Memorizing a programming language using spaced repetition'
+    );
+
+    await useCommandPalette(window, 'Incremental Reading: Import Article');
+    await window.getByRole('button', { name: 'Import' }).click();
+    await useCommandPalette(window, 'Incremental Reading: Learn');
+
+    const skipButton = window.getByRole('button', { name: 'Skip' });
+    await expect(skipButton).toBeInViewport();
+    await skipButton.click();
+
+    await expect(window.getByText('Nothing due for review.')).toBeVisible();
+    await useCommandPalette(window, 'Close current tab');
+
+    await useCommandPalette(window, 'Incremental Reading: Learn');
+    await expect(
+      window
+        .getByText(
+          'Memorizing a programming language using spaced repetition software'
+        )
+        .nth(3)
+    ).toBeVisible();
+  });
+
+  test('Can dismiss items from review UI', async () => {
+    await openNote(
+      window,
+      'sources/Memorizing a programming language using spaced repetition'
+    );
+
+    await useCommandPalette(window, 'Incremental Reading: Import Article');
+    await window.getByRole('button', { name: 'Import' }).click();
+    await useCommandPalette(window, 'Incremental Reading: Learn');
+
+    const dismissButton = window.getByRole('button', { name: 'Dismiss' });
+    await expect(dismissButton).toBeInViewport();
+    await dismissButton.click();
+
+    await useCommandPalette(window, 'Close current tab');
+
+    await useCommandPalette(window, 'Incremental Reading: Learn');
+    await expect(
+      window
+        .getByText(
+          'Memorizing a programming language using spaced repetition software'
+        )
+        .nth(3)
+    ).not.toBeVisible();
+  });
+
+  test('Can dismiss items from note pane', async () => {
+    await openNote(
+      window,
+      'sources/Memorizing a programming language using spaced repetition'
+    );
+
+    await useCommandPalette(window, 'Incremental Reading: Import Article');
+    await window.getByRole('button', { name: 'Import' }).click();
+
+    await openNote(
+      window,
+      'incremental-reading/articles/Memorizing a programming language using spaced repetition'
+    );
+
+    const dismissButton = window.getByRole('button', { name: 'Dismiss' });
+    await expect(dismissButton).toBeInViewport();
+    await dismissButton.click();
+
+    await useCommandPalette(window, 'Incremental Reading: Learn');
+    await expect(
+      window
+        .getByText(
+          'Memorizing a programming language using spaced repetition software'
+        )
+        .nth(3)
+    ).not.toBeVisible();
+  });
+
+  test('Can un-dismiss items from note pane', async () => {
+    await openNote(
+      window,
+      'sources/Memorizing a programming language using spaced repetition'
+    );
+
+    await useCommandPalette(window, 'Incremental Reading: Import Article');
+    await window.getByRole('button', { name: 'Import' }).click();
+
+    await openNote(
+      window,
+      'incremental-reading/articles/Memorizing a programming language using spaced repetition'
+    );
+
+    const dismissButton = window.getByRole('button', { name: 'Dismiss' });
+    await expect(dismissButton).toBeInViewport();
+    await dismissButton.click();
+
+    await useCommandPalette(window, 'Incremental Reading: Learn');
+    await expect(
+      window
+        .getByText(
+          'Memorizing a programming language using spaced repetition software'
+        )
+        .nth(3)
+    ).not.toBeVisible();
+
+    await useCommandPalette(window, 'Close current tab');
+
+    const unDismissButton = window.getByRole('button', { name: 'Un-dismiss' });
+    await expect(unDismissButton).toBeInViewport();
+    await unDismissButton.click();
+
+    await useCommandPalette(window, 'Incremental Reading: Learn');
+    await expect(window.locator('.ir-title')).toBeInViewport();
+  });
+
+  test.skip('Can change priority from the review pane', async () => {});
+});
+
+test.describe.skip('Extracting snippets', () => {
+  test('Can extract from Markdown notes', async () => {
+    await openNote(
+      window,
+      'sources/Memorizing a programming language using spaced repetition'
+    );
+  });
+
+  test('Can extract from articles in review interface', async () => {});
+
+  test('Can extract from snippets in review interface', async () => {});
 });
---
+++ src/tests/e2e/helpers.ts
@@ -0,0 +1,30 @@
+import type { Page } from '@playwright/test';
+
+// Reusable functions to execute Obsidian operations in tests
+
+export async function useCommandPalette(window: Page, command: string) {
+  await window.getByLabel('Open command palette', { exact: true }).click();
+  const commandPalette = window.getByRole('textbox', {
+    name: 'Select a command...',
+  });
+  await commandPalette.fill(command);
+  await commandPalette.press('Enter');
+}
+/**
+ * Opens a note in the current tab.
+ * TODO: Make more resilient (e.g., handle if the note is already open)
+ * @param path relative path using forward slashes. Do not enquote segments.
+ */
+export async function openNote(window: Page, path: string) {
+  await useCommandPalette(window, 'Quick switcher: Open quick switcher');
+  await window
+    .getByRole('textbox', { name: 'Find or create a note...' })
+    .fill(path);
+  await window
+    .locator('div')
+    .filter({
+      hasText: path,
+    })
+    .nth(1)
+    .click();
+}
