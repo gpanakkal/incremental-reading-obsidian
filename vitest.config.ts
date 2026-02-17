0053793e6fff8dab81be4a4d705de14d64a88919
gpanakkal <111669555+gpanakkal@users.noreply.github.com>
2026-02-17 11:30:50 -0800

    fix e2e test exclude pattern for vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vitest/config';
 // TOOD: alias react to preact https://preactjs.com/guide/v10/getting-started#aliasing-in-jest
 export default defineConfig({
   test: {
-    exclude: ['**/node_modules/**', '**/e2e/**'],
+    exclude: ['**/node_modules/**', '**/e2e-tests/**'],
   },
 });
