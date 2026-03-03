9edc8f64733a8ff1fae464cd222ccd8e4fbcf80a
gpanakkal <111669555+gpanakkal@users.noreply.github.com>
2026-03-02 17:42:59 -0800

    move seenIds state into Redux

--- src/hooks/useStore.tsx
+++ src/hooks/useStore.tsx
@@ -1,6 +1,11 @@
 import type { IRPluginState } from '#/lib/store';
 import { useSelector } from 'react-redux';
 
+/**
+ * Subscribes the calling component to state changes and triggers re-renders,
+ * but values can become stale. Use store.getState() as an escape hatch for
+ * current state if needed.
+ */
 export const useReduxStore = () => {
   return useSelector((state: IRPluginState) => state);
 };
