7e58997906c37b1032e91249d1ac8ff1ca5dac20
gpanakkal <111669555+gpanakkal@users.noreply.github.com>
2026-04-06 13:18:47 -0700

    chore: configure vitest to recognize ts config paths

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,9 @@
+import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vitest/config';
 
 // TOOD: alias react to preact https://preactjs.com/guide/v10/getting-started#aliasing-in-jest
 export default defineConfig({
+  plugins: [tsconfigPaths()],
   test: {
     exclude: ['**/node_modules/**', '**/e2e-tests/**'],
   },
