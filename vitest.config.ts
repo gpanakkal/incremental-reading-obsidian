66f33578e2c0deed49a0fafaaf4e94332ed9ecb5
gpanakkal <111669555+gpanakkal@users.noreply.github.com>
2026-05-21 19:10:32 -0700

    chore: remove tsconfigPaths vitest plugin

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,13 +1,12 @@
 import { resolve } from 'path';
-import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vitest/config';
 
 // TOOD: alias react to preact https://preactjs.com/guide/v10/getting-started#aliasing-in-jest
 export default defineConfig({
-  plugins: [tsconfigPaths()],
   resolve: {
     alias: {
       obsidian: resolve(__dirname, 'src/test/__mocks__/obsidian.ts'),
+      '#': resolve(__dirname, 'src'),
     },
   },
   test: {
