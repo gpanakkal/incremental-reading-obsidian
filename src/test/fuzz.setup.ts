5985038a9f897e8843f8ff02e6fd1f6156b92877
gpanakkal <111669555+gpanakkal@users.noreply.github.com>
2026-05-29 19:12:48 -0700

    chore: scale fuzzing interrupt time to number of runs

--- src/test/fuzz.setup.ts
+++ src/test/fuzz.setup.ts
@@ -1,9 +1,11 @@
 import fc from 'fast-check';
 
+const numRuns = parseInt(process.env.FC_NUM_RUNS ?? '10000', 10);
+
 fc.configureGlobal({
-  numRuns: parseInt(process.env.FC_NUM_RUNS ?? '10000', 10),
+  numRuns: numRuns,
   verbose: process.env.FC_VERBOSE !== 'false',
   endOnFailure: process.env.FC_END_ON_FAILURE !== 'false',
-  interruptAfterTimeLimit: 20_000,
+  interruptAfterTimeLimit: numRuns * 2,
   markInterruptAsFailure: true,
 });
