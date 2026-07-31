b459f40c77f255ea46d7bfa48f2c2b04b6811a57
gpanakkal <111669555+gpanakkal@users.noreply.github.com>
2026-07-30 17:53:35 -0700

    refactor: extract TextScheduler to separate file

--- src/components/action-bar/ActionBar.tsx
+++ src/components/action-bar/ActionBar.tsx
@@ -11,12 +11,10 @@ import {
   type ReviewCard,
   type ReviewSnippet,
 } from '#/lib/types';
-import { SchedulingModal } from '#/views/SchedulingModal';
 import {
   ArchiveRestore,
   Ban,
   BrainCog,
-  CalendarSync,
   Check,
   Eye,
   House,
@@ -31,6 +29,7 @@ import { ButtonWithIcon, TextButton } from './BarButtons';
 import { FixedIntervalField } from './FixedIntervalField';
 import { PriorityField } from './PriorityField';
 import { ReviewTypeFilter } from './ReviewTypeFilter';
+import { TextScheduler } from './TextScheduler';
 
 export function ActionBar() {
   const page = useAppSelector((state) => state.page);
@@ -177,51 +176,6 @@ function TextActions({ text }: { text: ReviewText }) {
         <SkipForward />
       </ButtonWithIcon>
       <TextScheduler text={text} />
-    </>
-  );
-}
-
-/**
- * Field to set the priority or fixed interval plus a button to open a
- * SchedulingModal
- */
-function TextScheduler({ text }: { text: ReviewText }) {
-  const { plugin, actions } = useReviewContext();
-  const strategy =
-    text.data.type === 'article' && text.data.fixed_interval_days !== null
-      ? 'fixed'
-      : 'priority';
-
-  return (
-    <>
-      {strategy === 'priority' && (
-        <PriorityField
-          key={text.data.id}
-          onBlur={async (priority: number) => {
-            await actions.reprioritize(text, priority);
-          }}
-          initialPriority={text.data.priority}
-        />
-      )}
-      {strategy === 'fixed' && (
-        <FixedIntervalField
-          key={text.data.id}
-          onBlur={async (intervalDays: number) => {
-            await actions.manageFixedInterval(text as ReviewArticle, {
-              newIntervalDays: intervalDays,
-            });
-          }}
-          initialInterval={(text as ReviewArticle).data.fixed_interval_days}
-        />
-      )}
-      <ButtonWithIcon
-        tooltip="Change scheduling strategy"
-        handleClick={() => {
-          new SchedulingModal(plugin, text).open();
-        }}
-      >
-        <CalendarSync />
-      </ButtonWithIcon>
     </>
   );
 }
