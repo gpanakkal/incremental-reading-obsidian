71271b25311f850db7b8364dc913b92173716039
gpanakkal <111669555+gpanakkal@users.noreply.github.com>
2026-09-15 15:12:18 -0700

    fix: bug preventing article review entries from being deleted when undoing review

--- src/lib/items/ArticleManager.ts
+++ src/lib/items/ArticleManager.ts
@@ -614,7 +614,7 @@ export class ArticleManager extends ItemManager {
       if (!reviewRow)
         throw new Error(`No article review found with ID "${reviewId}"`);
       await this.repo.mutate(
-        `DELETE FROM article_review WHERE article_id = $1 AND (id = $2 OR review_time > $3`,
+        `DELETE FROM article_review WHERE article_id = $1 AND (id = $2 OR review_time > $3)`,
         [originalArticle.id, reviewId, reviewRow.review_time]
       );
       await this.repo.mutate(
